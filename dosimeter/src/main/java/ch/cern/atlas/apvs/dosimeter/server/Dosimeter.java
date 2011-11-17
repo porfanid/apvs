e46dfd0c3b928abb1ca97dba79b787a9d77ee021
Mark Donszelmann <Mark.Donszelmann@gmail.com>
2011-11-17 23:47:58 +0100

    Generating dosimeter values

--- dosimeter/src/main/java/ch/cern/atlas/apvs/dosimeter/server/Dosimeter.java
+++ dosimeter/src/main/java/ch/cern/atlas/apvs/dosimeter/server/Dosimeter.java
@@ -24,7 +24,7 @@ public class Dosimeter {
 		double rate = dosimeter.getRate() / MICRO_SIEVERT * 100;
 		// FIXME...
 		int ratePower = 0;
-		String s = String.format("0%5d00%07.0f000%03.0f%1d0000000000000000000000", dosimeter.getSerialNo(), dosimeter.getDose(), rate, ratePower);
+		String s = String.format("0%05d00%07.0f000%03.0f%1d0000000000000000000000", dosimeter.getSerialNo(), dosimeter.getDose(), rate, ratePower);
 		int checksum = getChecksum(s);
 		return s + String.format("%02X", checksum);
 	}
@@ -81,16 +81,19 @@ public class Dosimeter {
 	
 	Dosimeter next() {
 		double newDose = getDose() + getRate();
-		double newRate = getRate() + random.nextGaussian() * 0.5;
+		double newRate = Math.max(0, getRate() + random.nextGaussian() * 0.5);
 		return new Dosimeter(getSerialNo(), newDose, newRate);
 	}
 	
 	private static int getChecksum(String encodedString) {
+		System.err.println(encodedString);
 		int checksum = 0;
 		for (int i=0; i<Math.min(encodedString.length(), 44); i+=2) {
 			checksum += Integer.parseInt(encodedString.substring(i, i+2), 16);
 		}
-		checksum = 0x100 - (checksum & 0xFF);
+		checksum = checksum & 0xFF;
+		checksum = 0x100 - checksum;
+		checksum = checksum & 0xFF;
 		return checksum;
 	}
 }
