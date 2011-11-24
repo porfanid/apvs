96fb1f3f6db4503109ca042769635306da5ba582
Mark Donszelmann <Mark.Donszelmann@gmail.com>
2011-11-24 22:57:56 +0100

    Finalized ptu reader and service

---
+++ ptu/src/main/java/ch/cern/atlas/apvs/ptu/server/PtuConstants.java
@@ -0,0 +1,8 @@
+package ch.cern.atlas.apvs.ptu.server;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public interface PtuConstants {
+	public static final DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+}
