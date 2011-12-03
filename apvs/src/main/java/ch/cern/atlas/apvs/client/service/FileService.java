ed5a7850c8b310a3006e4d610f942244eb19ecbe
Mark Donszelmann <Mark.Donszelmann@gmail.com>
2011-12-03 23:40:04 +0100

    Fixed Dosimeter View

---
+++ apvs/src/main/java/ch/cern/atlas/apvs/client/service/FileService.java
@@ -0,0 +1,12 @@
+package ch.cern.atlas.apvs.client.service;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+/**
+ * @author Mark Donszelmann
+ */
+@RemoteServiceRelativePath("apvsFile")
+public interface FileService extends RemoteService {
+	    
+}
