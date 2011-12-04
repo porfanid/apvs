0c157648d9ba1e3933d79cd2e886db6747dd05e2
Mark Donszelmann <Mark.Donszelmann@gmail.com>
2011-12-04 23:27:51 +0100

    Added icons and startup screens; landscape ipad startup does not work; bookmark icon does not work

--- apvs/src/main/java/ch/cern/atlas/apvs/client/MeasurementView.java
+++ apvs/src/main/java/ch/cern/atlas/apvs/client/MeasurementView.java
@@ -8,14 +8,14 @@ import ch.cern.atlas.apvs.client.event.SelectDosimeterEvent;
 import ch.cern.atlas.apvs.client.event.SelectPtuEvent;
 import ch.cern.atlas.apvs.domain.Dosimeter;
 import ch.cern.atlas.apvs.domain.Measurement;
-import ch.cern.atlas.apvs.domain.Ptu;
 import ch.cern.atlas.apvs.dosimeter.shared.DosimeterChangedEvent;
 import ch.cern.atlas.apvs.eventbus.shared.RemoteEventBus;
-import ch.cern.atlas.apvs.ptu.shared.PtuChangedEvent;
+import ch.cern.atlas.apvs.ptu.shared.MeasurementChangedEvent;
 
 import com.google.gwt.cell.client.Cell.Context;
 import com.google.gwt.cell.client.TextCell;
 import com.google.gwt.i18n.client.NumberFormat;
+import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.safehtml.shared.SafeHtmlUtils;
 import com.google.gwt.user.cellview.client.CellTable;
@@ -31,14 +31,14 @@ public class MeasurementView extends SimplePanel {
 
 	private static NumberFormat format = NumberFormat.getFormat("0.00");
 
-	private ListDataProvider<Measurement<?>> dataProvider = new ListDataProvider<Measurement<?>>();
-	private CellTable<Measurement<?>> table = new CellTable<Measurement<?>>();
-	private ListHandler<Measurement<?>> columnSortHandler;
+	Measurement<Double> last = new Measurement<Double>();
+	private ListDataProvider<Measurement<Double>> dataProvider = new ListDataProvider<Measurement<Double>>();
+	private CellTable<Measurement<Double>> table = new CellTable<Measurement<Double>>();
+	private ListHandler<Measurement<Double>> columnSortHandler;
 
 	private int dosimeterSerialNo = -1;
 	private Dosimeter dosimeter;
 	private int ptuId = -1;
-	private Ptu ptu;
 
 	public MeasurementView(final RemoteEventBus eventBus) {
 
@@ -104,35 +104,39 @@ public class MeasurementView extends SimplePanel {
 				}
 
 				ptuId = event.getPtuId();
-				ptu = null;
 
 				// register a new handler
 				if (ptuId >= 0) {
-					registration = PtuChangedEvent.subscribe(eventBus,
-							new PtuChangedEvent.Handler() {
+					registration = MeasurementChangedEvent.subscribe(eventBus,
+							new MeasurementChangedEvent.Handler() {
 
 								@Override
-								public void onPtuChanged(PtuChangedEvent event) {
-									if (event.getPtu().getPtuId() == ptuId) {
-										ptu = event.getPtu();
+								public void onMeasurementChanged(
+										MeasurementChangedEvent event) {
+									Measurement<Double> measurement = event
+											.getMeasurement();
+									if (measurement.getPtuId() == ptuId) {
+										last = replace(measurement);
 										update();
 									}
 								}
 							});
+				} else {
+					dataProvider.getList().clear();
 				}
 
 				update();
 			}
 		});
 
-		TextColumn<Measurement<?>> name = new TextColumn<Measurement<?>>() {
+		TextColumn<Measurement<Double>> name = new TextColumn<Measurement<Double>>() {
 			@Override
-			public String getValue(Measurement<?> object) {
+			public String getValue(Measurement<Double> object) {
 				return object.getName();
 			}
 
 			@Override
-			public void render(Context context, Measurement<?> object,
+			public void render(Context context, Measurement<Double> object,
 					SafeHtmlBuilder sb) {
 				String name = getValue(object);
 				if (name != null) {
@@ -145,55 +149,36 @@ public class MeasurementView extends SimplePanel {
 		name.setSortable(true);
 		table.addColumn(name, "Name");
 
-		TextColumn<Measurement<?>> value = new TextColumn<Measurement<?>>() {
+		TextColumn<Measurement<Double>> value = new TextColumn<Measurement<Double>>() {
 			@Override
-			public String getValue(Measurement<?> object) {
+			public String getValue(Measurement<Double> object) {
 				if (object == null) {
 					return "";
 				}
-				return format.format((Double) object.getValue());
+				return format.format(object.getValue());
 			}
 
 			@Override
-			public void render(Context context, Measurement<?> object,
+			public void render(Context context, Measurement<Double> object,
 					SafeHtmlBuilder sb) {
 				String s = getValue(object);
 				// FIXME does not work for dosimeter
-				if ((object != null) && (ptu != null)
-						&& object.getName().equals(ptu.getLastChanged())) {
-					String a;
-					switch (ptu.getState()) {
-					case NEW:
-						a = "&larr;";
-						break;
-					case UP:
-						a = "&uarr;";
-						break;
-					case DOWN:
-						a = "&darr;";
-						break;
-					default:
-						a = "";
-						break;
-					}
-					s = a + "&nbsp;<b>" + s + "</b>";
-				}
 				((TextCell) getCell()).render(context,
-						SafeHtmlUtils.fromSafeConstant(s), sb);
+						decorate(s, object, last), sb);
 			}
 
 		};
 		value.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_RIGHT);
 		table.addColumn(value, "Value");
 
-		TextColumn<Measurement<?>> unit = new TextColumn<Measurement<?>>() {
+		TextColumn<Measurement<Double>> unit = new TextColumn<Measurement<Double>>() {
 			@Override
-			public String getValue(Measurement<?> object) {
+			public String getValue(Measurement<Double> object) {
 				return object.getUnit();
 			}
 
 			@Override
-			public void render(Context context, Measurement<?> object,
+			public void render(Context context, Measurement<Double> object,
 					SafeHtmlBuilder sb) {
 				String unit = getValue(object);
 				if (unit != null) {
@@ -206,16 +191,16 @@ public class MeasurementView extends SimplePanel {
 		unit.setSortable(true);
 		table.addColumn(unit, "Unit");
 
-		List<Measurement<?>> list = new ArrayList<Measurement<?>>();
+		List<Measurement<Double>> list = new ArrayList<Measurement<Double>>();
 		dataProvider.addDataDisplay(table);
 		dataProvider.setList(list);
 
-		columnSortHandler = new ListHandler<Measurement<?>>(
+		columnSortHandler = new ListHandler<Measurement<Double>>(
 				dataProvider.getList());
 		columnSortHandler.setComparator(name,
-				new Comparator<Measurement<? extends Object>>() {
-					public int compare(Measurement<? extends Object> o1,
-							Measurement<? extends Object> o2) {
+				new Comparator<Measurement<Double>>() {
+					public int compare(Measurement<Double> o1,
+							Measurement<Double> o2) {
 						if (o1 == o2) {
 							return 0;
 						}
@@ -228,27 +213,24 @@ public class MeasurementView extends SimplePanel {
 					}
 				});
 		columnSortHandler.setComparator(value,
-				new Comparator<Measurement<?>>() {
-					public int compare(Measurement<?> o1, Measurement<?> o2) {
+				new Comparator<Measurement<Double>>() {
+					public int compare(Measurement<Double> o1,
+							Measurement<Double> o2) {
 						if (o1 == o2) {
 							return 0;
 						}
 
-						if ((o1 != null)
-								&& (o1.getValue() instanceof Comparable<?>)) {
-							@SuppressWarnings("unchecked")
-							Comparable<Object> value = (Comparable<Object>) o1
-									.getValue();
-							return (o2 != null) ? value.compareTo(o2.getValue())
-									: 1;
+						if (o1 != null) {
+							return (o2 != null) ? o1.getValue().compareTo(
+									o2.getValue()) : 1;
 						}
 						return -1;
 					}
 				});
 		columnSortHandler.setComparator(unit,
-				new Comparator<Measurement<? extends Object>>() {
-					public int compare(Measurement<? extends Object> o1,
-							Measurement<? extends Object> o2) {
+				new Comparator<Measurement<Double>>() {
+					public int compare(Measurement<Double> o1,
+							Measurement<Double> o2) {
 						if (o1 == o2) {
 							return 0;
 						}
@@ -264,24 +246,52 @@ public class MeasurementView extends SimplePanel {
 		table.getColumnSortList().push(name);
 	}
 
+	public static SafeHtml decorate(String s, Measurement<Double> current,
+			Measurement<Double> last) {
+		if ((current != null) && (last != null)
+				&& (current.getPtuId() == last.getPtuId())
+				&& current.getName().equals(last.getName())) {
+			double c = current.getValue();
+			double l = last.getValue();
+			String a = (c == l) ? "&larr;" : (c > l) ? "&uarr;" : "&darr;";
+			s = a + "&nbsp;<b>" + s + "</b>";
+		}
+		return SafeHtmlUtils.fromSafeConstant(s);
+	}
+
 	private void update() {
 		// FIXME maybe a better way
-		List<Measurement<?>> list = dataProvider.getList();
-		list.clear();
-
-		if (ptu != null) {
-			list.addAll(ptu.getMeasurements());
-		}
-
 		if (dosimeter != null) {
-			list.add(new Measurement<Double>("Radiation Rate", dosimeter
-					.getRate(), "&micro;Sv/h"));
-			list.add(new Measurement<Double>("Radiation Dose", dosimeter
-					.getDose(), "&micro;Sv"));
+			replace(new Measurement<Double>("Radiation Rate",
+					dosimeter.getRate(), "&micro;Sv/h"));
+			replace(new Measurement<Double>("Radiation Dose",
+					dosimeter.getDose(), "&micro;Sv"));
 		}
 
 		// Re-sort the table
 		ColumnSortEvent.fire(table, table.getColumnSortList());
 		table.redraw();
+	}
+
+	private Measurement<Double> replace(Measurement<Double> measurement) {
+		List<Measurement<Double>> list = dataProvider.getList();
+		
+		int i = 0;
+		while (i < list.size()) {
+			if (list.get(i).getName().equals(measurement.getName())) {
+				break;
+			}
+			i++;
+		}
+
+		Measurement<Double> lastValue;
+		if (i == list.size()) {
+			list.add(measurement);
+			lastValue = measurement;
+		} else {
+			lastValue = list.set(i, measurement);
+		}
+	
+		return lastValue;
 	}
 }
