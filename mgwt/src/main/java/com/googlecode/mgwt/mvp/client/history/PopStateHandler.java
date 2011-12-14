e3a8e0b48d745772a0b0b0e6872785814b29dd15
Mark Donszelmann <Mark.Donszelmann@gmail.com>
2011-12-14 22:35:58 +0100

    Upgraded to mgwt 1.0.1

--- mgwt/src/main/java/com/googlecode/mgwt/mvp/client/history/PopStateHandler.java
+++
@@ -1,7 +0,0 @@
-package com.googlecode.mgwt.mvp.client.history;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface PopStateHandler extends EventHandler {
-	public void onPopStateEvent(PopStateEvent event);
-}
