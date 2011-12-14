1b9ead28a112512d9566c379be3d3af70c3094bf
Mark Donszelmann <Mark.Donszelmann@gmail.com>
2011-12-14 09:25:24 +0100

    Image resize

--- apvs/src/main/java/ch/cern/atlas/apvs/client/tablet/ImagePanel.java
+++ apvs/src/main/java/ch/cern/atlas/apvs/client/tablet/ImagePanel.java
@@ -1,25 +1,30 @@
 package ch.cern.atlas.apvs.client.tablet;
 
+import com.google.gwt.event.logical.shared.AttachEvent;
+import com.google.gwt.event.logical.shared.ResizeEvent;
+import com.google.gwt.event.logical.shared.ResizeHandler;
+import com.google.gwt.event.logical.shared.AttachEvent.Handler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Image;
 import com.googlecode.mgwt.dom.client.event.tap.HasTapHandlers;
 import com.googlecode.mgwt.ui.client.MGWT;
 import com.googlecode.mgwt.ui.client.widget.Button;
 import com.googlecode.mgwt.ui.client.widget.LayoutPanel;
 
-public class ImagePanel extends DetailPanel implements
-		ImageUI {
+public class ImagePanel extends DetailPanel implements ImageUI {
 
 	private LayoutPanel panel;
 	private Button button;
 
 	public ImagePanel(String url) {
 
-		Image image = new Image(url);
+		final Image image = new Image(url);
 		image.setWidth("100%");
-		// FIXME, make either width or height set to 100% depending on the ratio of the image and the ratio of the panel.
-		
-//		image.setHeight("100%");
-		
+		// FIXME, make either width or height set to 100% depending on the ratio
+		// of the image and the ratio of the panel.
+
+		// image.setHeight("100%");
+
 		panel = new LayoutPanel();
 		panel.add(image);
 
@@ -32,9 +37,38 @@ public class ImagePanel extends DetailPanel implements
 		}
 
 		scrollPanel.setWidget(panel);
-		
+
 		// width 100%
 		scrollPanel.setScrollingEnabledX(false);
+
+		scrollPanel.addAttachHandler(new Handler() {
+
+			@Override
+			public void onAttachOrDetach(AttachEvent event) {
+				System.err.println("A " + scrollPanel.getOffsetWidth() + " "
+						+ scrollPanel.getOffsetHeight());
+				System.err.println("P " + panel.getOffsetWidth() + " "
+						+ panel.getOffsetHeight());
+				System.err.println("I " + image.getWidth() + " "
+						+ image.getHeight());
+			}
+		});
+
+		Window.addResizeHandler(new ResizeHandler() {
+
+			@Override
+			public void onResize(ResizeEvent event) {
+				if (scrollPanel.getOffsetHeight() > 0) {
+					System.err.println("A* "
+							+ ((double)scrollPanel.getOffsetWidth() / scrollPanel
+									.getOffsetHeight()));
+				}
+				if (image.getHeight() > 0) {
+					System.err.println("I* "
+							+ ((double)image.getWidth() / image.getHeight()));
+				}
+			}
+		});
 	}
 
 	@Override
