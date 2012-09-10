182ba4a0985f4e47f98c94fc168739f90d2b4f85
Mark Donszelmann <Mark.Donszelmann@cern.ch>
2012-09-10 16:28:50 +0200

    Changed Date formatting for shorter d/M/yyyy instead of dd/MM/yyyy

--- apvs-ptu/src/main/java/ch/cern/atlas/apvs/ptu/server/PtuConstants.java
+++ apvs-ptu/src/main/java/ch/cern/atlas/apvs/ptu/server/PtuConstants.java
@@ -4,5 +4,5 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 public interface PtuConstants {
-	public static final DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	public static final DateFormat dateFormat = new SimpleDateFormat("d/M/yyyy HH:mm:ss");
 }
