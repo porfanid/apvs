a602bdc8607d22a72742743f7e305837d078e065
Mark Donszelmann <Mark.Donszelmann@cern.ch>
2012-09-18 11:13:24 +0200

    Increase frameID at every frame

--- apvs-ptu/src/main/java/ch/cern/atlas/apvs/ptu/server/JsonHeader.java
+++ apvs-ptu/src/main/java/ch/cern/atlas/apvs/ptu/server/JsonHeader.java
@@ -4,12 +4,15 @@ import ch.cern.atlas.apvs.domain.Measurement;
 
 public class JsonHeader {
 
+	transient static int currentFrameID = 0;
+	
 	String sender;
 	String receiver = "Broadcast";
-	String frameID = "0";
+	String frameID;
 	String acknowledge = "False"; 
 	JsonMessage[] messages;
 	
+/*
 	public JsonHeader(String sender, String receiver, String frameID, String acknowledge, JsonMessage[] messages) {
 		this.sender = sender;
 		this.receiver = receiver;
@@ -17,8 +20,10 @@ public class JsonHeader {
 		this.acknowledge = acknowledge;
 		this.messages = messages;
 	}
-	
+*/	
 	public JsonHeader(Measurement<Double> measurement) {
+		currentFrameID++;
+		frameID = String.valueOf(currentFrameID);
 		sender = measurement.getPtuId();
 		messages = new JsonMessage[1];
 		messages[0] = new JsonMessage(measurement);
