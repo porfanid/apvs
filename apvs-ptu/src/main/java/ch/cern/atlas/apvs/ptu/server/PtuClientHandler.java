7ce8dd743c6b18f1bd73d139666a4b1914ae6e18
Mark Donszelmann <Mark.Donszelmann@cern.ch>
2012-09-18 16:17:05 +0200

    Progress on #112, #113, #114

--- apvs-ptu/src/main/java/ch/cern/atlas/apvs/ptu/server/PtuClientHandler.java
+++ apvs-ptu/src/main/java/ch/cern/atlas/apvs/ptu/server/PtuClientHandler.java
@@ -26,10 +26,13 @@ import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.channel.ChannelStateEvent;
 import org.jboss.netty.channel.MessageEvent;
 
+import ch.cern.atlas.apvs.domain.Error;
+import ch.cern.atlas.apvs.domain.Event;
 import ch.cern.atlas.apvs.domain.History;
 import ch.cern.atlas.apvs.domain.Measurement;
 import ch.cern.atlas.apvs.domain.Message;
 import ch.cern.atlas.apvs.domain.Ptu;
+import ch.cern.atlas.apvs.domain.Report;
 import ch.cern.atlas.apvs.eventbus.shared.RemoteEventBus;
 import ch.cern.atlas.apvs.eventbus.shared.RequestRemoteEvent;
 import ch.cern.atlas.apvs.ptu.shared.MeasurementChangedEvent;
@@ -67,8 +70,7 @@ public class PtuClientHandler extends PtuReconnectHandler {
 				} else if (type.equals(MeasurementChangedEvent.class.getName())) {
 					List<Measurement> m = getMeasurements();
 					System.err.println("Getting all meas " + m.size());
-					for (Iterator<Measurement> i = m.iterator(); i
-							.hasNext();) {
+					for (Iterator<Measurement> i = m.iterator(); i.hasNext();) {
 						eventBus.fireEvent(new MeasurementChangedEvent(i.next()));
 					}
 				}
@@ -104,106 +106,126 @@ public class PtuClientHandler extends PtuReconnectHandler {
 		for (Iterator<Message> i = list.iterator(); i.hasNext();) {
 			Message message = i.next();
 			if (message instanceof Measurement) {
-				@SuppressWarnings("unchecked")
-				Measurement measurement = (Measurement) message;
-
-				String ptuId = measurement.getPtuId();
-				Ptu ptu = ptus.get(ptuId);
-				if (ptu == null) {
-					ptu = new Ptu(ptuId);
-					ptus.put(ptuId, ptu);
-					ptuIdsChanged = true;
-				}
-
-				String sensor = measurement.getName();
-				History history = ptu.getHistory(sensor);
-				if (history == null) {
-					// check on history and load from DB
-					if ((historyQuery != null) && (historyQueryCount != null)) {
-
-						long PERIOD = 36; // hours
-						Date then = new Date(new Date().getTime() - (PERIOD * 3600000));
-						String timestamp = PtuConstants.timestampFormat.format(then);
-						
-						try {
-							historyQueryCount.setString(1, sensor);
-							historyQueryCount.setString(2, ptuId);
-							historyQueryCount.setString(3, timestamp);
-							historyQuery.setString(1, sensor);
-							historyQuery.setString(2, ptuId);
-							historyQuery.setString(3, timestamp);
-
-							ResultSet result = historyQueryCount.executeQuery();
-							result.next();
-
-							int n = result.getInt(1);
-							result.close();
-
-							int MAX_ENTRIES = 1000;
-							long MIN_INTERVAL = 5000; // ms
-
-							if (n > 0) {
-								// limit entries
-								if (n > MAX_ENTRIES)
-									n = 1000;
-
-								Deque<Number[]> data = new ArrayDeque<Number[]>(
-										n);
-
-								long lastTime = new Date().getTime();
-								result = historyQuery.executeQuery();
-								while (result.next() && (data.size() <= n)) {
-									long time = result.getTimestamp(1)
-											.getTime();
-
-									// limit entry separation (reverse order
-									// !!!)
-									if (lastTime - time > MIN_INTERVAL) {
-										lastTime = time;
-
-										Number[] entry = new Number[2];
-										entry[0] = time;
-										entry[1] = Double.parseDouble(result
-												.getString(2));
-										data.addFirst(entry);
-									}
-								}
-								result.close();
-
-								System.err.println("Creating history for "
-										+ ptuId + " " + sensor + " "
-										+ data.size() + " entries");
-								history = new History(
-										data.toArray(new Number[data.size()][]),
-										measurement.getUnit());
-
-								ptu.setHistory(sensor, history);
-							}
-						} catch (SQLException ex) {
-							System.err.println(ex);
-						}
-					}
-				}
-
-				ptu.addMeasurement(measurement);
-				Set<String> changed = measurementChanged.get(ptuId);
-				if (changed == null) {
-					changed = new HashSet<String>();
-					measurementChanged.put(ptuId, changed);
-				}
-				changed.add(measurement.getName());
-
-				// duplicate entries will not be added
-				if (history != null) {
-					history.addEntry(measurement.getDate().getTime(),
-							measurement.getValue());
-				}
-				
-				sendEvents();
+				handleMessage((Measurement)message); 
+			} else if (message instanceof Report) {
+				handleMessage((Report)message); 				
+			} else if (message instanceof Event) {
+				handleMessage((Event)message); 								
+			} else if (message instanceof Error) {
+				handleMessage((Error)message); 								
+			} else {
+				System.err.println("Error: unknown Message Type: "+message.getType());
 			}
 		}
 	}
 
+	private void handleMessage(Measurement measurement) {
+
+		String ptuId = measurement.getPtuId();
+		Ptu ptu = ptus.get(ptuId);
+		if (ptu == null) {
+			ptu = new Ptu(ptuId);
+			ptus.put(ptuId, ptu);
+			ptuIdsChanged = true;
+		}
+
+		String sensor = measurement.getName();
+		History history = ptu.getHistory(sensor);
+		if (history == null) {
+			// check on history and load from DB
+			if ((historyQuery != null) && (historyQueryCount != null)) {
+
+				long PERIOD = 36; // hours
+				Date then = new Date(new Date().getTime() - (PERIOD * 3600000));
+				String timestamp = PtuConstants.timestampFormat.format(then);
+
+				try {
+					historyQueryCount.setString(1, sensor);
+					historyQueryCount.setString(2, ptuId);
+					historyQueryCount.setString(3, timestamp);
+					historyQuery.setString(1, sensor);
+					historyQuery.setString(2, ptuId);
+					historyQuery.setString(3, timestamp);
+
+					ResultSet result = historyQueryCount.executeQuery();
+					result.next();
+
+					int n = result.getInt(1);
+					result.close();
+
+					int MAX_ENTRIES = 1000;
+					long MIN_INTERVAL = 5000; // ms
+
+					if (n > 0) {
+						// limit entries
+						if (n > MAX_ENTRIES)
+							n = 1000;
+
+						Deque<Number[]> data = new ArrayDeque<Number[]>(n);
+
+						long lastTime = new Date().getTime();
+						result = historyQuery.executeQuery();
+						while (result.next() && (data.size() <= n)) {
+							long time = result.getTimestamp(1).getTime();
+
+							// limit entry separation (reverse order
+							// !!!)
+							if (lastTime - time > MIN_INTERVAL) {
+								lastTime = time;
+
+								Number[] entry = new Number[2];
+								entry[0] = time;
+								entry[1] = Double.parseDouble(result
+										.getString(2));
+								data.addFirst(entry);
+							}
+						}
+						result.close();
+
+						System.err
+								.println("Creating history for " + ptuId + " "
+										+ sensor + " " + data.size()
+										+ " entries");
+						history = new History(data.toArray(new Number[data
+								.size()][]), measurement.getUnit());
+
+						ptu.setHistory(sensor, history);
+					}
+				} catch (SQLException ex) {
+					System.err.println(ex);
+				}
+			}
+		}
+
+		ptu.addMeasurement(measurement);
+		Set<String> changed = measurementChanged.get(ptuId);
+		if (changed == null) {
+			changed = new HashSet<String>();
+			measurementChanged.put(ptuId, changed);
+		}
+		changed.add(measurement.getName());
+
+		// duplicate entries will not be added
+		if (history != null) {
+			history.addEntry(measurement.getDate().getTime(),
+					measurement.getValue());
+		}
+
+		sendEvents();
+	}
+
+	private void handleMessage(Report report) {
+		System.err.println(report.getType()+" NOT YET IMPLEMENTED");
+	}
+	
+	private void handleMessage(Event event) {
+		System.err.println(event.getType()+" NOT YET IMPLEMENTED");
+	}
+	
+	private void handleMessage(Error error) {
+		System.err.println(error.getType()+" NOT YET IMPLEMENTED");
+	}
+	
 	private void init() {
 		ptus = new TreeMap<String, Ptu>();
 	}
@@ -257,7 +279,8 @@ public class PtuClientHandler extends PtuReconnectHandler {
 			historyQueryCount = connection
 					.prepareStatement("select count(*) from tbl_measurements "
 							+ "join tbl_devices on tbl_measurements.device_id = tbl_devices.id "
-							+ "where sensor = ? " + "and name = ? " + "and datetime > timestamp ?");
+							+ "where sensor = ? " + "and name = ? "
+							+ "and datetime > timestamp ?");
 
 			historyQuery = connection
 					.prepareStatement("select DATETIME, VALUE from tbl_measurements "
