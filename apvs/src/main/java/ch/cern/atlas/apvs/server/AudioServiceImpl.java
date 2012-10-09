236174b9bb5d6904fe238a173a78ac36221ad446
aalvesdo <a.alvsantos@cern.ch>
2012-10-09 20:02:45 +0200

    Fixed #183

--- apvs/src/main/java/ch/cern/atlas/apvs/server/AudioServiceImpl.java
+++ apvs/src/main/java/ch/cern/atlas/apvs/server/AudioServiceImpl.java
@@ -1,14 +1,11 @@
 package ch.cern.atlas.apvs.server;
 
 import java.io.IOException;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
