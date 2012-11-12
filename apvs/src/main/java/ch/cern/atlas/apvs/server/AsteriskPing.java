38daf956694fae3607f35cd1c7c17fd5aac21e33
aalvesdo <a.alvsantos@cern.ch>
2012-11-12 21:20:18 +0100

    Ping Asterisk Server

---
+++ apvs/src/main/java/ch/cern/atlas/apvs/server/AsteriskPing.java
@@ -0,0 +1,46 @@
+package ch.cern.atlas.apvs.server;
+
+import java.io.IOException;
+
+import org.asteriskjava.manager.ManagerConnection;
+import org.asteriskjava.manager.TimeoutException;
+import org.asteriskjava.manager.action.PingAction;
+import org.asteriskjava.manager.response.ManagerResponse;
+import org.asteriskjava.manager.response.PingResponse;
+
+public class AsteriskPing {
+
+	private PingAction ping;
+	private ManagerConnection serverConnection;
+
+	public AsteriskPing(ManagerConnection managerConnection) {
+		this.serverConnection = managerConnection;
+		ping = new PingAction();
+	}
+
+	public boolean isAlive() {
+		ManagerResponse response = new ManagerResponse();
+		try {
+			response = this.serverConnection.sendAction(ping, 10000);
+
+		} catch (IllegalArgumentException e1) {
+			System.out.println("Exception thrown in pooling server: "
+					+ e1.getMessage());
+		} catch (IllegalStateException e1) {
+			System.out.println("Exception thrown in pooling server: "
+					+ e1.getMessage());
+		} catch (IOException e1) {
+			System.out.println("Exception thrown in pooling server: "
+					+ e1.getMessage());
+		} catch (TimeoutException e1) {
+			System.out.println("Exception thrown in pooling server: "
+					+ e1.getMessage());
+		}
+		if (response instanceof PingResponse) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
