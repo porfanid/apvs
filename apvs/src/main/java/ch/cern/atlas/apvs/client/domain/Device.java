bc0fadeeb292fd311cfb57b9d11cdd2b476e0a9f
Mark Donszelmann <Mark.Donszelmann@cern.ch>
2013-01-16 16:14:14 +0100

    Fixed #395 and fixed #396

--- apvs/src/main/java/ch/cern/atlas/apvs/client/domain/Device.java
+++ apvs/src/main/java/ch/cern/atlas/apvs/client/domain/Device.java
@@ -17,16 +17,20 @@ public class Device implements Serializable {
 	
 	private String ip;
 	private String description;
+	private String macAddress;
+	private String hostName;
 	
 	public Device() {
 		// Serialization
 	}
 	
-	public Device(int id, String name, String ip, String description) {
+	public Device(int id, String name, String ip, String description, String macAddress, String hostName) {
 		this.id = id;
 		this.name = name;
 		this.ip = ip;
 		this.description = description;
+		this.macAddress = macAddress;
+		this.hostName = hostName;
 	}
 
 	public int getId() {
@@ -43,5 +47,13 @@ public class Device implements Serializable {
 
 	public String getDescription() {
 		return description;
+	}
+	
+	public String getMacAddress() {
+		return macAddress;
+	}
+	
+	public String getHostName() {
+		return hostName;
 	}
 }
