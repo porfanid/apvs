5037a2d795fe1a251e346ae0a809035361839011
Mark Donszelmann <Mark.Donszelmann@cern.ch>
2013-02-08 14:30:43 +0100

    Progress on #289

--- apvs/src/main/java/ch/cern/atlas/apvs/client/ui/AbstractTimeView.java
+++ apvs/src/main/java/ch/cern/atlas/apvs/client/ui/AbstractTimeView.java
@@ -32,15 +32,18 @@ import org.moxieapps.gwt.highcharts.client.plotOptions.SeriesPlotOptions;
 import ch.cern.atlas.apvs.client.widget.GlassPanel;
 import ch.cern.atlas.apvs.ptu.shared.PtuClientConstants;
 
+import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.event.logical.shared.ResizeEvent;
 import com.google.gwt.event.logical.shared.ResizeHandler;
 import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.i18n.client.NumberFormat;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
 
 public class AbstractTimeView extends GlassPanel {
 
-	private static final int POINT_LIMIT = 2000; // 5 seconds basis, 12 pnts/min, 720/hour
+	private static final int POINT_LIMIT = 2000; // 5 seconds basis, 12
+													// pnts/min, 720/hour
 	private static final String[] color = { "#AA4643", "#89A54E", "#80699B",
 			"#3D96AE", "#DB843D", "#92A8CD", "#A47D7C", "#B5CA92", "#4572A7" };
 	private Map<String, Integer> pointsById;
@@ -59,13 +62,13 @@ public class AbstractTimeView extends GlassPanel {
 		seriesById = new HashMap<String, Series>();
 		colorsById = new HashMap<String, String>();
 		limitSeriesById = new HashMap<String, Series>();
-		
+
 		Window.addResizeHandler(new ResizeHandler() {
-			
+
 			@Override
 			public void onResize(ResizeEvent event) {
 				if (chart != null) {
-					chart.setSizeToMatchContainer();
+					chart.setSize(300, chart.getOffsetHeight(), false);
 				}
 			}
 		});
@@ -98,8 +101,8 @@ public class AbstractTimeView extends GlassPanel {
 						"#92A8CD", "#A47D7C", "#B5CA92", "#4572A7")
 				.setZoomType(Chart.ZoomType.X)
 				.setSizeToMatchContainer()
-			    .setWidth100()
-			    .setHeight100()
+				.setWidth100()
+				.setHeight100()
 				.setChartTitle(
 						title ? new ChartTitle().setText(name).setStyle(
 								new Style().setFontSize("12px")) : null)
