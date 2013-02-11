35404477557bd7598a3d6cacf1b5e3ea528d5762
aalvesdo <a.alvsantos@cern.ch>
2013-02-11 14:40:21 +0100

    Audio Default URL fix

--- apvs/src/main/java/ch/cern/atlas/apvs/client/settings/ServerSettings.java
+++ apvs/src/main/java/ch/cern/atlas/apvs/client/settings/ServerSettings.java
@@ -77,7 +77,7 @@ public class ServerSettings extends AbstractServerSettings {
 		put(Entry.ptuUrl.toString(), "pcatlaswpss03:10123");
 		put(Entry.procedureUrl.toString(), "http://localhost:8890/apvs-procs/procedures");
 		put(Entry.databaseUrl.toString(), "wpss@//pcatlaswpss03.cern.ch:1521/XE");
-		put(Entry.audioUrl.toString(), "atlas@pcatlaswpss02.cern.ch:5038");
+		put(Entry.audioUrl.toString(), "manager@pcatlaswpss02.cern.ch");
 	}
 	
 	public String toString() {
