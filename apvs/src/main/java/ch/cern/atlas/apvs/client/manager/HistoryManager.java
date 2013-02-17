51e29e37d3774e36ff160793b9189c6a4081c78f
Mark Donszelmann <Mark.Donszelmann@cern.ch>
2013-02-17 14:31:12 +0100

    Debug for #502

--- apvs/src/main/java/ch/cern/atlas/apvs/client/manager/HistoryManager.java
+++ apvs/src/main/java/ch/cern/atlas/apvs/client/manager/HistoryManager.java
@@ -63,7 +63,7 @@ public class HistoryManager {
 
 		long now = new Date().getTime();
 		// #502, reduce to one hour for now
-		Date yesterday = new Date(now - (/* 24 */ 60 * 60 * 1000));
+		Date yesterday = new Date(now - (/* 24 */ 2 * 60 * 60 * 1000));
 
 		historyMap = new HistoryMap();
 		clientFactory.getPtuService().getHistoryMap(ptuIds, yesterday,
