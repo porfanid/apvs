d4fc303b98977ec8d1e411b38fd7dc1c2d140dc8
Mark Donszelmann <Mark.Donszelmann@cern.ch>
2013-04-09 15:56:24 +0200

    Fixed #590

--- apvs-player/src/main/java/ch/cern/atlas/apvs/player/APVSPlayer.java
+++ apvs-player/src/main/java/ch/cern/atlas/apvs/player/APVSPlayer.java
@@ -1,27 +1,17 @@
 package ch.cern.atlas.apvs.player;
 
-import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.Cookies;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.rpc.StatusCodeException;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.RootPanel;
-
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import org.moxieapps.gwt.highcharts.client.Chart;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.RootPanel;
 
 public class APVSPlayer implements EntryPoint {
 
     static final Logger logger = Logger.getLogger(APVSPlayer.class.getName());
-
-    int count = 0;
-    
-    Button startButton;
-    Button stopButton;
     
     @Override
     public void onModuleLoad() {
@@ -33,23 +23,11 @@ public class APVSPlayer implements EntryPoint {
             }
         });
 
-        Button button = new Button("Broadcast");
-        button.addClickHandler(new ClickHandler() {
-            @Override
-            public void onClick(ClickEvent event) {
-            }
-        });
 
-        Button post = new Button("Post");
-        post.addClickHandler(new ClickHandler() {
-            @Override
-            public void onClick(ClickEvent event) {
-            }
-        });
-
-        Button pollButton = new Button("Poll");
-
-        RootPanel.get().add(button);
+//        StockChart chart = new StockChart();
+        Chart chart = new Chart();
+        
+        RootPanel.get().add(chart);
     
     }
     
