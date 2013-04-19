805362e5e3a196dd238f1792ae717781bdc33239
Mark Donszelmann <Mark.Donszelmann@cern.ch>
2013-04-19 11:46:05 +0200

    Added daq server (setup)

---
+++ apvs-daq-server/src/main/java/ch/cern/atlas/apvs/daq/server/DaqServer.java
@@ -0,0 +1,8 @@
+package ch.cern.atlas.apvs.daq.server;
+
+public class DaqServer {
+
+	public static void main(String[] args) {
+		System.out.println("DaqServer...");
+	}
+}
---
+++ apvs-daq-server/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+java -jar target/apvs-daq-server-jar-with-dependencies.jar
+
+
