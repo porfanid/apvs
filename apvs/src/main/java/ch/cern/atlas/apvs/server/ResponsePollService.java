61d07df224ddad3a5b5e92415b904d8b854f4fa6
Mark Donszelmann <Mark.Donszelmann@cern.ch>
2013-06-19 11:31:01 +0200

    Possible for for #630

--- apvs/src/main/java/ch/cern/atlas/apvs/server/ResponsePollService.java
+++ apvs/src/main/java/ch/cern/atlas/apvs/server/ResponsePollService.java
@@ -1,10 +1,17 @@
 package ch.cern.atlas.apvs.server;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.atmosphere.gwt.poll.AtmospherePollService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gwt.user.server.rpc.SerializationPolicy;
 
 @SuppressWarnings("serial")
 public class ResponsePollService extends AtmospherePollService {
-
+	private Logger log = LoggerFactory.getLogger(getClass().getName());
+	
 	public ResponsePollService() {
 	}
 	
@@ -16,5 +23,13 @@ public class ResponsePollService extends AtmospherePollService {
 	@Override
 	public SuspendInfo suspend(long timeout) {
 		return super.suspend(timeout);
+	}
+	
+	@Override
+	protected SerializationPolicy doGetSerializationPolicy(
+			HttpServletRequest request, String moduleBaseURL, String strongName) {
+		// TODO Auto-generated method stub
+		log.info("***** "+moduleBaseURL);
+		return super.doGetSerializationPolicy(request, moduleBaseURL, strongName);
 	}
 }
