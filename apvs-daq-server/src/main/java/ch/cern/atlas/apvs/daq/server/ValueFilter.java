bd1c793056b66d89669be3b718ef973ad7d94688
Natascia <ndebortoli@gmail.com>
2013-08-16 11:37:19 +0200

    changes on FilterTest.java

--- apvs-daq-server/src/main/java/ch/cern/atlas/apvs/daq/server/ValueFilter.java
+++ apvs-daq-server/src/main/java/ch/cern/atlas/apvs/daq/server/ValueFilter.java
@@ -1,7 +1,7 @@
 package ch.cern.atlas.apvs.daq.server;
 
 import java.util.List;
-
+  
 import ch.cern.atlas.apvs.domain.Measurement;
 
 public class ValueFilter implements Filter {
@@ -10,7 +10,38 @@ public class ValueFilter implements Filter {
 	public boolean filter(Measurement current, List<Measurement> list,
 			double resolution) {
 		// TODO Auto-generated method stub
-		return false;
+		int lengthOfList=list.size();
+		System.out.println("ValueFilter initial lengthOfList = " + lengthOfList);
+		boolean res;
+		if (lengthOfList < 2){
+			//with 0 or 1 previous measurement in LIST, just add current as new element of list.
+			list.add(current);
+			//return false
+			res = false;
+		} else{
+			Measurement lastMeasurement = list.get(lengthOfList-1);
+			Measurement secondLastMeasurement = list.get(lengthOfList-2);
+			double currentValue = current.getValue();
+			if ((!isInThreshold(lastMeasurement.getValue(), currentValue, resolution)) || 
+			   (!isInThreshold(secondLastMeasurement.getValue(), lastMeasurement.getValue(), resolution))){
+				// current value is over resolution-distance from last measurement or from second last measurement: 
+				// it has to be added as new measurement
+				System.out.println("ValueFilter: add <"+current.getValue()+"> to the list");
+			    list.add(current);
+			    // return false;
+			    res = false;
+			} else {
+				// current measurement is inside the resolution-distance from last and second last measurements: 
+				// update the timestamp
+				lastMeasurement.setDate(current.getDate());
+				//return true;
+				res = true;
+			}
+		}
+		return res;
 	}
-
+	
+	private static boolean isInThreshold(double v1, double v2, double distance){
+		return Math.abs(v1-v2)<=distance;
+	}
 }
--- apvs-daq-server/src/test/java/ch/cern/atlas/apvs/daq/server/FilterTest.java
+++ apvs-daq-server/src/test/java/ch/cern/atlas/apvs/daq/server/FilterTest.java
@@ -24,7 +24,7 @@ public class FilterTest {
 	Date t3 = new Date(now+80000);
 	Date t4 = new Date(now+100000);
 	Date t5 = new Date(now+120000);
-	
+
 	double v0 = 0.1;
 	double v1 = 0.1;
 	double v2 = 0.1;
@@ -32,11 +32,11 @@ public class FilterTest {
 	double v4 = 0.14;
 	double v5 = 0.2;
 	double r = 0.05;
-	
+
 	Measurement m0, m1, m2, m3, m4, m5;
 
 	List<Measurement> l;
-	
+
 	@Before
 	public void before() {
 		m0 = new Temperature(ptuId, v0, t0);
@@ -47,39 +47,43 @@ public class FilterTest {
 		m5 = new Temperature(ptuId, v5, t5);
 		l = new ArrayList<Measurement>();
 	}
-	
-//	@Test
+
+	@Test
 	public void initialValue() {
+		System.out.println("FilterTest 1");
 		boolean b = f.filter(m0, l, r);
 		Assert.assertFalse(b);
 		Assert.assertEquals(1, l.size());
 		Assert.assertEquals(m0, l.get(0));
 	}
-	
-//	@Test
+
+	@Test
 	public void twoTimesSameValue() {
+		System.out.println("FilterTest 2");
 		f.filter(m0, l, r);
-		boolean b = f.filter(m1, l, r);
+		boolean b = f.filter(m1, l, r);		
 		Assert.assertFalse(b);
 		Assert.assertEquals(2, l.size());
 		Assert.assertEquals(m0, l.get(0));
 		Assert.assertEquals(m1, l.get(1));
 	}
 
-//	@Test
+	@Test
 	public void threeTimesSameValue() {
+		System.out.println("FilterTest 3 ");
 		f.filter(m0, l, r);
 		f.filter(m1, l, r);
-		boolean b = f.filter(m2, l, r);
-		Assert.assertTrue(b);
+		boolean b = f.filter(m2, l, r);		
+		Assert.assertTrue("Wrong return ", b);
 		Assert.assertEquals(2, l.size());
 		Assert.assertEquals(m0, l.get(0));
 		Assert.assertEquals(m1.getValue(), l.get(1).getValue());
 		Assert.assertEquals(m2.getDate(), l.get(1).getDate());
 	}
-	
-//	@Test
+
+	@Test
 	public void valueAboveResulotion() {
+		System.out.println("FilterTest 4 ");
 		f.filter(m0, l, r);
 		f.filter(m1, l, r);
 		f.filter(m2, l, r);
@@ -90,9 +94,10 @@ public class FilterTest {
 		Assert.assertEquals(m2, l.get(1));
 		Assert.assertEquals(m5, l.get(2));
 	}
-	
-//	@Test
+
+	@Test
 	public void valueBelowResolution() {
+		System.out.println("FilterTest 5");
 		f.filter(m0, l, r);
 		f.filter(m1, l, r);
 		f.filter(m2, l, r);
@@ -104,9 +109,10 @@ public class FilterTest {
 		Assert.assertEquals(m2.getValue(), l.get(2).getValue());
 		Assert.assertEquals(m3.getDate(), l.get(2).getDate());
 	}
-	
+
 //	@Test
 	public void secondValueAboveResolution() {
+		System.out.println("FilterTest 6");
 		f.filter(m0, l, r);
 		f.filter(m1, l, r);
 		f.filter(m2, l, r);
@@ -119,9 +125,10 @@ public class FilterTest {
 		Assert.assertEquals(m3.getDate(), l.get(1).getDate());
 		Assert.assertEquals(m5, l.get(2));
 	}
-	
+
 //	@Test
 	public void secondValueBelowResolution() {
+		System.out.println("FilterTest 7");
 		f.filter(m0, l, r);
 		f.filter(m1, l, r);
 		f.filter(m2, l, r);
@@ -133,9 +140,10 @@ public class FilterTest {
 		Assert.assertEquals(m2.getValue(), l.get(1).getValue());
 		Assert.assertEquals(m4.getDate(), l.get(1).getDate());
 	}
-		
+
 //	@Test
 	public void disconnectAfterLastValue() {
+		System.out.println("FilterTest 8");
 		f.filter(m0, l, r);
 		f.filter(m1, l, r);
 		f.filter(m2, l, r);
@@ -145,9 +153,10 @@ public class FilterTest {
 		Assert.assertEquals(2, l.size());
 		Assert.assertFalse(l.get(l.size()-1).isConnected());
 	}
-		
-	@Test
+
+	//@Test
 	public void discardChange() {
+		System.out.println("FilterTest 9");
 		Filter d = new Filter() {
 
 			@Override
@@ -156,12 +165,13 @@ public class FilterTest {
 				return false;
 			}
 		};
-		
+
 		Assert.assertFalse(d.filter(m0, l, r));
 	}
-	
-	@Test
+
+	//@Test
 	public void addChange() {
+		System.out.println("FilterTest 10");
 		Filter a = new Filter() {
 
 			@Override
@@ -173,35 +183,37 @@ public class FilterTest {
 		};
 
 		boolean update = a.filter(m0, l, 0.001);
-		
+
 		Assert.assertFalse(update);
 		Assert.assertEquals(1, l.size());
 	}
-	
-	@Test
+
+	//@Test
 	public void updateChange() {
+		System.out.println("FilterTest 11");
 		Filter u = new Filter() {
 
 			@Override
 			public boolean filter(Measurement current, List<Measurement> list,
 					double resolution) {
-				
+
 				// update date
 				list.get(list.size()-1).setDate(current.getDate());
 				return true;
 			}
 		};
-		
+
 		l.add(m0);
 		boolean update = u.filter(m1, l, 0.001);
-		
+
 		Assert.assertTrue(update);
 		Assert.assertEquals(1, l.size());
 		Assert.assertEquals(t1, l.get(l.size()-1).getDate());
 	}
-	
-	@Test
+
+	//@Test
 	public void disconnectChange() {
+		System.out.println("FilterTest 12");
 		Filter d = new Filter() {
 
 			@Override
@@ -211,13 +223,13 @@ public class FilterTest {
 				return true;
 			}
 		};
-		
+
 		l.add(m0);
 		boolean update = d.filter(m1, l, 0.001);
 		Assert.assertTrue(update);
 		Assert.assertEquals(1, l.size());
 		Assert.assertFalse(l.get(l.size()-1).isConnected());
 	}
-	
+
 
 }
