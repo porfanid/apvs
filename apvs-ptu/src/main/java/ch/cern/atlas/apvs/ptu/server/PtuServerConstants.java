16d0010ca7165a0a1edb7f2a602e2d367bbda7ce
Mark Donszelmann <Mark.Donszelmann@cern.ch>
2013-10-07 14:36:44 +0200

    Partial fix for TimeZone #726

--- apvs-ptu/src/main/java/ch/cern/atlas/apvs/ptu/server/PtuServerConstants.java
+++ apvs-ptu/src/main/java/ch/cern/atlas/apvs/ptu/server/PtuServerConstants.java
@@ -2,9 +2,16 @@ package ch.cern.atlas.apvs.ptu.server;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 
-public interface PtuServerConstants {
+public class PtuServerConstants {
+	public static final TimeZone timeZone = TimeZone.getTimeZone("CEST");
 	public static final DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 	public static final DateFormat timestampFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	public static final String oracleFormat = "'YYYY-MM-DD HH24:MI:SS'";
+	
+	static {
+		dateFormat.setTimeZone(timeZone);
+		timestampFormat.setTimeZone(timeZone);
+	}
 }
