709b69eea44aad4ce3b340cb6b0afe16a58c70f2
Mark Donszelmann <Mark.Donszelmann@cern.ch>
2013-10-08 14:10:33 +0200

    Possible fix for timezone on gwt

--- apvs-domain/src/main/java/ch/cern/atlas/apvs/domain/ClientConstants.java
+++ apvs-domain/src/main/java/ch/cern/atlas/apvs/domain/ClientConstants.java
@@ -4,7 +4,7 @@ import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.i18n.client.TimeZone;
 
 public interface ClientConstants {
-	public static final TimeZone timeZone = TimeZone.createTimeZone("CEST");
+	public static final TimeZone timeZone = TimeZone.createTimeZone("Europe/Zurich");
 	public static final DateTimeFormat simpleDateFormat = DateTimeFormat.getFormat("dd/MM/yyyy HH:mm:ss");
 	public static final DateTimeFormat dateFormat = DateTimeFormat.getFormat("dd MMM yyyy HH:mm:ss");
 	public static final DateTimeFormat dateFormatNoSeconds = DateTimeFormat.getFormat("dd MMM yyyy HH:mm");
